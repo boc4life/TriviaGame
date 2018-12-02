b3f65f48b2bcebb0eb78921a612d6836a71470a1
boc4life <lheandrew@gmail.com>
2018-12-02 17:58:30 -0500

    pushing nonworking code for help

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,7 @@ var timeoutGuess = 0;
 var currentQuestion = 0;
 var game;
 var remainingQuestions = 0;
+var activeQuestion = false;
 
 var christmasGame = {
     questions: [
@@ -25,51 +26,82 @@ var christmasGame = {
         picture: ""
     }
 ],
-    correctAnswers: ["Sleepy", "Brenda Lee"]
+    correctAnswers: ["Sleepy", "Brenda Lee"],
+    css: "",
+    title: "<h1>Christmas Trivia!</h1>"
 }
-  $(document).ready(function(){
+
+$(document).ready(function(){
+    
+    // Start game when dropdown is selected, assign the correct set of properties to the game variable
     $(".selector").on("click", function(){
         game = eval($(this).attr("value"));
         startGame();
-        insertTimer();
     })
+    
+    // Execute for correct and incorrect selections, only while a question is active to prevent errant clicks in between questions from causing problems
+
+    
   })
 
-  function startGame() {
+  if (activeQuestion) {
+    $(".option").on("click", function(){
+    remainingQuestions--;
+    console.log(remainingQuestions);
+    selectedAnswer = $(this).text();
+    if (selectedAnswer == game.correctAnswers[currentQuestion]) {
+        correctGuess++;
+        $("#question").html("Correct!");
+        $(".option").empty();
+        $("#choice1").html(game.questions[currentQuestion].fact);
+        $("#choice2").html("<img src=" + game.questions[currentQuestion].picture + ">");
+    }
+    else {
+        incorrectGuess++;
+        $("#question").html("Wrong!");
+        $(".option").empty();
+        $("#choice1").html(game.questions[currentQuestion].fact);
+        $("#choice2").html("<img src=" + game.questions[currentQuestion].picture + ">");
+    }
+    activeQuestion = false;
+    currentQuestion++;
+    setTimeout(nextQuestion, 5000);
+
+})
+}
+
+function startGame() {
     correctGuess = 0;
     incorrectGuess = 0;
     timeoutGuess = 0;
     currentQuestion = 0;
     remainingQuestions = game.questions.length;
+    console.log(remainingQuestions);
 
+    $("#timebox").html("Time Remaining: <span id=\"timer\">30</span> Seconds");
     $("#welcome").empty();
     $("#topics").empty();
+    $("#question").empty();
+    $("#gameTitle").html(game.title);
+    nextQuestion();
   }
-    
-  function nextQuestion() {
+
+  // Advance to next question. If no questions remaining, execute end of game function.
+function nextQuestion() {
+    if (remainingQuestions = 0) {
+        endGame();
+    }
+    else {
+    activeQuestion = true;
     $("#question").html(game.questions[currentQuestion].display);
     $("#choice1").html(game.questions[currentQuestion].choice1).attr("value", game.questions[currentQuestion].choice1);
     $("#choice2").html(game.questions[currentQuestion].choice2).attr("value", game.questions[currentQuestion].choice2);
     $("#choice3").html(game.questions[currentQuestion].choice3).attr("value", game.questions[currentQuestion].choice3);
     $("#choice4").html(game.questions[currentQuestion].choice4).attr("value", game.questions[currentQuestion].choice4);
-
-    $(".option").on("click", function(){
-        selectedAnswer = $(this).text();
-        if (selectedAnswer == game.correctAnswers[currentQuestion]) {
-            alert("Correct!");
-            correctGuess++;
-            $("#question").html("Correct!");
-            $(".option").empty();
-            $("#choice1").html(game.questions[currentQuestion].fact);
-            $("#choice2").html("<img src=" + game.questions[currentQuestion].picture + ">")
-        }
-        else {
-            alert("Sorry, incorrect")
-            incorrectGuess++;
-        }
-    })
+    // startTimer();
+    console.log(activeQuestion);
   }
+}
 
-  function insertTimer() {
-    $("#timebox").html("Time Remaining: <span id=\"timer\">30</span> Seconds");
-  }
+
+
